d7f870d9e71cec6a29ead9072c31963ca10da7bd
Bruce Giovando <bgiovand@gmail.com>
2022-11-25 17:49:01 -0800

    about page almost there

--- pages/about.jsx
+++ pages/about.jsx
@@ -177,6 +177,7 @@ export default function About() {
         </section>
 
         <section>
+          <H2 title="The Principals" />
           <div className="mx-20">
             {team.map((principal, index) => (
               <div key={principal.name} className="mx-10 my-20">
@@ -208,7 +209,7 @@ export default function About() {
           </div>
         </section>
 
-        <section className="flex flex-col items-center justify-center bg-stablesBlue p-10 text-stablesBrown">
+        <section className="flex flex-col items-center justify-center bg-stablesBlue px-10 py-24 text-stablesBrown">
           <p className="font-med text-4xl mx-28 leading-snug">{about.intro}</p>
         </section>
 
@@ -248,7 +249,7 @@ export default function About() {
             {testimonials.map((testimonial) => (
               <li
                 key={testimonial.name}
-                className="flex flex-col items-center m-5 w-1/3"
+                className="flex flex-col items-center m-5 md:w-1/3"
               >
                 <p className="text-2xl italic font-light text-stablesBlue my-3">
                   &#8220;{testimonial.quote}&#8221;
@@ -266,9 +267,7 @@ export default function About() {
                   <span className="text-stablesYellow text-2xl block">
                     {testimonial.name}
                   </span>
-                  <span className="">
-                    {testimonial.company}
-                  </span>
+                  <span className="">{testimonial.company}</span>
                 </div>
               </li>
             ))}
@@ -279,16 +278,11 @@ export default function About() {
           <H2 title="Quality &amp; Automation" />
 
           <div className="flex flex-col">
-            <p className="font-semibold text-2xl">
+            <p className="font-semibold text-2xl w-8/12 text-center my-5 m-auto">
               Everyone says they have the best slow burn cones but, is it really
               true if said quality isn’t consistent. With our expertise in
               manufacturing we can assure you that our quality is the most
               consistent.
-            </p>
-            <p className="font-light text-2xl">
-              Universal compatibility with automation equipment. We’ve done
-              rigorous testing on the most common equipment to make sure there’s
-              no disruption to your product lines.
             </p>
           </div>
 
@@ -296,11 +290,11 @@ export default function About() {
             Certifications
           </h3>
           <div className="mx-auto">
-            <ul className="flex flex-col md:flex-row">
+            <ul className="flex flex-col md:flex-row justify-center align-middle">
               {certifications.map((cert) => (
                 <li
                   key={cert.name}
-                  className="flex flex-col items-center m-5 flex-1 justify-top text-center"
+                  className="flex flex-col items-center justify-top text-center m-5 md:w-2/12"
                 >
                   {/* {cert.type} */}
                   <svg
@@ -316,9 +310,9 @@ export default function About() {
                     <path d="M10.819,13.295c1.695-2.503,3.78-4.723,6.17-6.567,.967,.715,1.994,1.364,3.074,1.941-1.809,2.017-3.431,4.252-3.699,4.626h1.086c.651-.879,1.999-2.646,3.421-4.212,1.553,.763,3.204,1.389,4.928,1.864-.459,.973-.806,1.823-1.009,2.348h.943c.217-.539,.535-1.292,.931-2.123,2.198,.536,4.5,.838,6.856,.884v1.239h.876v-1.236c2.339-.017,4.629-.282,6.818-.784,.374,.79,.675,1.503,.883,2.02h.943c-.195-.503-.523-1.308-.955-2.232,1.753-.457,3.437-1.065,5.021-1.816,1.367,1.524,2.645,3.2,3.274,4.048h1.086c-.26-.363-1.797-2.483-3.544-4.453,1.201-.621,2.339-1.327,3.404-2.114,2.39,1.844,4.476,4.064,6.17,6.567h1.054C53.37,5.3,44.372,0,34.157,0S14.944,5.3,9.764,13.295h1.055Zm15.371-3.149c-1.64-.442-3.212-1.023-4.695-1.733,.202-.209,.404-.411,.603-.603,2.029-1.959,5.368-4.107,6.1-4.57,.594,.326,1.242,.601,1.931,.822-.535,.701-1.587,2.126-2.731,3.934-.446,.703-.852,1.443-1.208,2.15m7.33,1.034c-2.217-.045-4.386-.321-6.459-.813,.324-.634,.686-1.285,1.076-1.903,1.319-2.082,2.509-3.645,2.906-4.155,.791,.187,1.621,.305,2.477,.343v6.528Zm.876,.004V4.662c.844-.015,1.669-.104,2.46-.264,.454,.587,1.587,2.0941,2.836,4.066,.411,.65,.791,1.338,1.128,2.003-2.066,.456-4.222,.7-6.424,.717m7.302-.925c-.371-.741-.797-1.523-1.265-2.263-1.086-1.714-2.088-3.085-2.645-3.821,.732-.206,1.425-.473,2.06-.798,1.028,.659,4.011,2.627,5.884,4.433,.249,.239,.5,.495,.752,.761-1.513,.698-3.116,1.265-4.786,1.688m8.895-4.078c-1.028,.742-2.126,1.407-3.282,1.99-.322-.346-.649-.681-.97-.992-1.752-1.688-4.361-3.462-5.638-4.2939,.434-.28,.83-.592,1.184-.931,3.164,.904,6.1,2.35,8.706,4.227M34.157,.876c2.313,0,4.561,.282,6.713,.81-1.633,1.326-4.08,2.105-6.713,2.105-2.643,0-5.083-.776-6.715-2.104,2.154-.529,4.402-.811,6.715-.811m-7.73,1.078c.288,.276,.604,.534,.945,.772-1.158,.746-4.009,2.647-5.882,4.453-.27,.26-.542,.537-.813,.824-1.036-.543-2.024-1.151-2.954-1.822,2.605-1.877,5.541-3.323,8.704-4.227" />
                     <path d="M57.499,44.796c-1.66,2.451-3.693,4.63-6.02,6.449-1.108-.833-2.298-1.578-3.556-2.228,1.516-1.71,2.873-3.531,3.376-4.221h-1.09c-.673,.899-1.85,2.421-3.101,3.8181-1.585-.7531-3.267-1.361-5.021-1.817,.37-.795,.663-1.498,.863-2.001h-.947c-.2,.4901-.466,1.111-.787,1.789-2.188-.5-4.478-.767-6.819-.784v-1.005h-.876v1.009c-2.357,.047-4.66,.346-6.856,.883-.343-.718-.627-1.377-.837-1.892h-.946c.208,.527,.521,1.275,.916,2.116-1.722,.476-3.374,1.099-4.928,1.864-1.305-1.439-2.548-3.046-3.247-3.98h-1.09c.519,.712,1.953,2.636,3.531,4.395-1.137,.609-2.216,1.295-3.227,2.054-2.326-1.819-4.36-3.998-6.019-6.449h-1.052c5.18,7.995,14.178,13.295,24.392,13.295s19.213-5.3,24.393-13.295h-1.052Zm-15.8,2.8051c1.67,.424,3.274,.989,4.787,1.688-.252,.265-.504,.522-.752,.763-1.868,1.8-4.843,3.762-5.877,4.426-.636-.325-1.329-.593-2.064-.799,.558-.737,1.557-2.105,2.64-3.814,.47-.742,.895-1.523,1.266-2.264m-7.302-.924c2.203,.016,4.359,.259,6.424,.717-.338,.664-.716,1.353-1.128,2.002-1.247,1.97-2.379,3.475-2.834,4.062-.788-.156-1.614-.245-2.462-.259v-6.522Zm-.876,.005v6.527c-.859,.038-1.691,.153-2.481,.338-.399-.513-1.585-2.071-2.902-4.151-.391-.617-.753-1.27-1.077-1.905,2.072-.489,4.24-.765,6.46-.809m-7.332,1.031c.358,.709,.764,1.447,1.209,2.152,1.145,1.806,2.194,3.231,2.73,3.933-.69,.218-1.337,.494-1.931,.822-.734-.464-4.07-2.613-6.099-4.568-.199-.193-.4-.395-.602-.605,1.482-.711,3.054-1.29,4.693-1.7339m-8.624,4.082c.976-.717,2.017-1.365,3.112-1.938,.27,.287,.544,.564,.813,.825,1.873,1.806,4.724,3.705,5.882,4.451-.412,.289-.789,.607-1.123,.951-3.159-.926-6.088-2.391-8.684-4.289m16.593,5.42c-2.388,0-4.707-.299-6.923-.862,1.618-1.421,4.187-2.283,6.923-2.283,2.733,0,5.305,.863,6.923,2.283-2.216,.563-4.535,.862-6.923,.862m7.909-1.131c-.398-.41-.851-.783-1.358-1.113,1.28-.833,3.883-2.603,5.632-4.289,.323-.312,.648-.647,.972-.993,1.214,.614,2.365,1.317,3.439,2.106-2.596,1.898-5.526,3.363-8.685,4.289" />
                   </svg>
-                  <div className="text-2xl font-semibold text-gray-100 px-3">
+                  <span className="text-2xl font-semibold text-gray-100">
                     {cert.name}
-                  </div>
+                  </span>
                 </li>
               ))}
             </ul>
@@ -327,6 +321,13 @@ export default function About() {
           <h3 className="text-stablesYellow text-5xl  text-center font-normal m-20 mb-10">
             Compatible Equipment
           </h3>
+          <p className="font-semibold text-2xl w-8/12 text-center  my-5 m-auto">
+            Universal compatibility with automation equipment.
+          </p>
+          <p className="font-light text-2xl w-8/12 text-center  my-5 m-auto">
+            We’ve done rigorous testing on the most common equipment to make
+            sure there’s no disruption to your product lines.
+          </p>
           <ul className="font-light flex flex-col md:flex-row flex-3 flex-wrap mx-auto justify-center">
             {compatibility.map((brand) => (
               <li
