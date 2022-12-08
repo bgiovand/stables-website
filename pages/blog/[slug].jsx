67a3a031cedc8d0b7d6f7bcb074bf996116dd02b
Bruce Giovando <bgiovand@gmail.com>
2022-12-08 11:38:28 -0800

    blog fix

--- pages/blog/[slug].jsx
+++ pages/blog/[slug].jsx
@@ -4,20 +4,24 @@ import Header from "../../src/components/Header"
 import Footer from "../../src/components/Footer"
 import groq from "groq"
 import imageUrlBuilder from "@sanity/image-url"
-import { PortableText } from "@portabletext/react"
+// import { PortableText } from "@portabletext/react"
 import client from "../../src/utils/client"
 import { H1 } from "@/components/Typography"
+import PortableText from "react-portable-text"
 
 const Post = ({ post = {} }) => {
-  const {
-    title = "Missing title",
-    name = "Missing name",
-    categories,
-    authorImage = "",
-    mainImage = "",
-    publishedAt = "",
-    body = [],
-  } = post
+ const {
+   title = "Missing title",
+   name = "Missing name",
+   categories = [],
+   authorImage = "",
+   mainImage = "",
+   published = new Date(post.publishedAt).toISOString() || "Missing date",
+   publishedAgo = "5 days ago",
+   relativePublishedDate = "",
+   body = [],
+   readTime = "",
+ } = post
 
 const ptComponents = {
   types: {
@@ -39,6 +43,42 @@ const ptComponents = {
   },
 }
 
+ const ptSerializers = {
+   h1: (props) => (
+     <h1 className="text-4xl text-stablesOrange mb-3" {...props} />
+   ),
+   h2: (props) => (
+     <h2 className="text-3xl text-stablesOrange mb-3" {...props} />
+   ),
+   h3: (props) => (
+     <h3 className="text-2xl text-stablesOrange mb-3" {...props} />
+   ),
+   h4: (props) => <h4 className="text-xl text-stablesOrange mb-3" {...props} />,
+   h5: (props) => <h5 className="text-lg text-stablesOrange mb-3" {...props} />,
+   normal: (props) => (
+     <p className="text-base text-gray-200 my-3 block" {...props} />
+   ),
+   span: (props) => <span className="text-base text-gray-500" {...props} />,
+   li: ({ children }) => <li className="color-[red]">{children}</li>,
+   ul: (props) => <ul className="text-base text-gray-200" {...props} />,
+   ol: (props) => <ol className="text-base text-gray-200" {...props} />,
+   link: (props) => (
+     <a
+       className="text-base text-stablesOrange"
+       target="_blank"
+       rel="noopener noreferrer"
+       {...props}
+     />
+   ),
+   strong: (props) => <strong className="text-base text-gray-200" {...props} />,
+   em: (props) => <em className="text-base text-gray-200" {...props} />,
+   del: (props) => <del className="text-base text-gray-200" {...props} />,
+   code: (props) => <code className="text-base text-gray-200" {...props} />,
+   blockquote: (props) => (
+     <blockquote className="text-base text-[red]" {...props} />
+   ),
+ }
+
   return (
     <main className="bgTexture">
       <Head>
@@ -51,8 +91,8 @@ const ptComponents = {
       <article style={{}} className={``}>
         <div className="flex flex-col items-center justify-center w-8/12 mx-auto py-40">
           <H1 title={title} />
-          {publishedAt && (
-            <div className="text-sm text-gray-500">{publishedAt}</div>
+          {published && (
+            <div className="text-sm text-gray-500">{published}</div>
           )}
 
           {authorImage && (
@@ -68,17 +108,30 @@ const ptComponents = {
           <span>{name}</span>
 
           {categories && (
-            <ul>
-              Posted in
+            <ul className="flex">
               {categories.map((category) => (
-                <li key={category}>{category}</li>
+                <li
+                  key={category}
+                  className="rounded-lg bg-stablesYellow/30 px-2 py-1 mx-1"
+                >
+                  {category}
+                </li>
               ))}
             </ul>
           )}
         </div>
 
         <div className="flex flex-col w-8/12 mx-auto bg-stablesBlack text-white px-20 py-20 leading-loose font-light text-lg shadow-lg h-screen">
-          <PortableText value={body} components={ptComponents} />
+          {/* <PortableText value={body} components={ptComponents} />
+           */}
+          <PortableText
+            content={body}
+            serializers={ptSerializers}
+            imageOptions={{ w: 320, h: 240, fit: "max" }}
+            components={ptComponents}
+            projectId="hwmnpy3d"
+            dataset="production"
+          />
         </div>
       </article>
 
